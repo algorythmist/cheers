2f13b23d7d0345a230dc7ce5495661613fa3d5e3
algorythmist <dimitri@tecacet.com>
2022-07-21 09:17:19 -0700

    generate tables

--- cheers.py
+++ cheers.py
@@ -1,19 +1,33 @@
 import math
 
 
-def recursive1(number_of_guests: int):
+def list_solutions_recursive1(number_of_guests: int) -> list:
     solution = [0, 0, 1, 1]
     for n in range(4, number_of_guests + 1):
         solution.append(math.ceil((n-1)/2) + solution[n-1])
     return solution
 
 
-def recursive2(number_of_guests: int):
+def list_solutions_recursive2(number_of_guests: int) -> list:
     solution = [0, 0, 1, 1, 3]
     for n in range(5, number_of_guests + 1):
         solution.append(n-1 + solution[n-2])
     return solution
 
 
-def explicit(number_of_guests: int):
-    return [0, 0, 1] + [(2*n**2-1+(-1)**n)//8 - 1 for n in range(3, number_of_guests + 1)]
+def number_of_clinks(number_of_guests: int) -> list:
+    n = number_of_guests
+    return (2*n**2-1+(-1)**n)//8 - 1
+
+
+def list_solutions_closed_form(number_of_guests: int) -> list:
+    return [0, 0, 1] + [ number_of_clinks(n) for n in range(3, number_of_guests + 1)]
+
+
+def solutions_in_table(number_of_guests: list) -> str:
+    content = '| Guests | Clinks |\n'
+    for n in number_of_guests:
+        content += f'| {n} | {number_of_clinks(n)} |\n'
+    return content
+
+
--- tests/test_cheers.py
+++ tests/test_cheers.py
@@ -6,13 +6,29 @@ from cheers import *
 class CheersTestCase(unittest.TestCase):
 
     def test_recursive1(self):
-        solutions = recursive1(15)
-        print(solutions)
+        solutions = list_solutions_recursive1(15)
+        self.assertEqual([0, 0, 1, 1, 3, 5, 8, 11, 15, 19, 24, 29, 35, 41, 48, 55], solutions)
 
     def test_recursive2(self):
-        solutions = recursive2(15)
-        print(solutions)
+        solutions = list_solutions_recursive2(15)
+        self.assertEqual([0, 0, 1, 1, 3, 5, 8, 11, 15, 19, 24, 29, 35, 41, 48, 55], solutions)
 
-    def test_explicit(self):
-        print(explicit(15))
+    def test_closed_form(self):
+        self.assertEqual([0, 0, 1, 1, 3, 5, 8, 11, 15, 19, 24, 29, 35, 41, 48, 55],
+                         list_solutions_closed_form(15))
 
+    def test_number_of_clinks(self):
+        self.assertEqual(24, number_of_clinks(10))
+        self.assertEqual(2499, number_of_clinks(100))
+        self.assertEqual(249999, number_of_clinks(1000))
+        self.assertEqual(24999999, number_of_clinks(10000))
+        self.assertEqual(2499999999, number_of_clinks(100000))
+        self.assertEqual(249999999999, number_of_clinks(1000000))
+
+    def test_generate_first_few(self):
+        table = solutions_in_table(range(3, 13))
+        print(table)
+
+    def test_generate_powers_of_10(self):
+        table = solutions_in_table([10 ** i for i in range(1, 10)])
+        print(table)
